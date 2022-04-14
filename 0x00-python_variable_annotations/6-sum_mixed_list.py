73954334befcdea3df507f58573a80f0ce7db15d
aloicerains <aloiceokoth98@gmail.com>
2022-04-14 18:16:24 +0300

    task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Mixed list Module"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    summation: float = 0
+    for i in mxd_list:
+        summation += i
+    return summation
