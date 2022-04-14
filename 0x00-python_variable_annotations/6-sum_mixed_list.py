fd4fddbe9d05363c2328c2fcb0a6298c1bdc2a14
aloicerains <aloiceokoth98@gmail.com>
2022-04-14 18:21:23 +0300

    edited docs

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,6 +5,10 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """Summation function
+    Return:
+        summation(float): summation of list
+    """
     summation: float = 0
     for i in mxd_list:
         summation += i
